9b49d2703040d18e1947059e90f8077437496edc
Darren Gresch <darrengresch@gmail.com>
2018-04-09 18:48:59 -0400

    Empty firing method with input setup leftmouse click

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -55,14 +55,8 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
 	{
 		auto AimDirection = OutLaunchVelocity.GetSafeNormal();
 		MoveBarrelToward(AimDirection);
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: Aim solution found"), Time);
 	}
-	else
-	{
-		auto Time = GetWorld()->GetTimeSeconds();
-		UE_LOG(LogTemp, Warning, TEXT("%f: No aim solution found"), Time);
-	}
+	// If no solution found, do nothing
 }
 
 void UTankAimingComponent::MoveBarrelToward(FVector AimDirection)
